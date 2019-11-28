83d27e63cd8c47bbdd1e2a74516b4014bba48db8
unknown <abeerailyas@uop.edu.pkgit config --global user.name abeera>
2019-11-28 10:43:17 +0500

    this file is part of coursera capstone project

---
+++ Coursera_capstone.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1>Capstone_Project</h1>
+# <h3>This notebook would be mainly used for the capstone project. This project serves as the final assignment for the IBM data science certification</h3>
+
+# In[2]:
+
+
+#importing the header files
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print ('hello Captstone Project Course')
+
+
+# In[ ]:
+
+
+
+
